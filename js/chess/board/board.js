2c3e24186932e5c3a20d4a22d6cd505577506ce7
David Chu <thedavidchu@gmail.com>
2020-12-29 13:26:27 -0500

    Fixed board

--- js/chess/board/board.js
+++ js/chess/board/board.js
@@ -3,13 +3,13 @@ The class for a chess game.
 
 board indices = 
 	[ 0,  1,  2,  3,  4,  5,  6,  7],
-    [ 8,  9, 10, 11, 12, 13, 14, 15],
-    [16, 17, 18, 19, 20, 21, 22, 23],
-    [24, 25, 26, 27, 28, 29, 30, 31],
-    [32, 33, 34, 35, 36, 37, 38, 39],
-    [40, 41, 42, 43, 44, 45, 46, 47],
-    [48, 49, 50, 51, 52, 53, 54, 55],
-    [56, 57, 58, 59, 60, 61, 62, 63]])
+	[ 8,  9, 10, 11, 12, 13, 14, 15],
+	[16, 17, 18, 19, 20, 21, 22, 23],
+	[24, 25, 26, 27, 28, 29, 30, 31],
+	[32, 33, 34, 35, 36, 37, 38, 39],
+	[40, 41, 42, 43, 44, 45, 46, 47],
+	[48, 49, 50, 51, 52, 53, 54, 55],
+	[56, 57, 58, 59, 60, 61, 62, 63]])
 
 ## Known bugs
 	1. Castle through check
@@ -21,11 +21,12 @@ class Board {
 	constructor(array=null) {
 		this.board = [];
 		this.prev_move = [null, null];
+		this.turn = 1;
 		this.castle = [true, true, true, true];		// [black left castle, black right castle, white left castle, white right castle], from white's perspective
 
 		if (array == null) {
 			this.board = [ -50, -30, -31, -90, -1000, -31, -30, -50, 
-						   -10, -10, -10, -10,   -10, -10, -10, -10, 
+							-10, -10, -10, -10,   -10, -10, -10, -10, 
 							 0,   0,   0,   0,     0,   0,   0,   0, 
 							 0,   0,   0,   0,     0,   0,   0,   0, 
 							 0,   0,   0,   0,     0,   0,   0,   0, 
@@ -43,7 +44,7 @@ class Board {
 		/**
 		Steps:
 			1. Check if there's a piece at i
-			2. Check if legal move (check if en pasant, check pawn promotion)
+			2. Check if legal move (check if en passant, check pawn promotion)
 			3. Update castle
 			4. Update previous move
 			5. Update board
@@ -75,6 +76,8 @@ class Board {
 		:param promotion: what to promot pawn to - NOTE: sign must be correct!
 		*/
 		let piece = this.board[i];
+		this.prev_move = [i, j];
+		this.turn *= -1;
 
 		// Check if castle
 		if (piece == 1000) {						// Range = [R, 57, 58, 59, K, 61, 62, R]
@@ -87,32 +90,32 @@ class Board {
 				if (j == 2) {this.#raw_move(0, 3);}
 				else if (j == 6) {this.#raw_move(7, 5);}
 			}
-		// Check en pasant and promotion
+		// Check en passant and promotion
 		} else if (piece == 10) {
-			// En pasant
-			if (24 <= i <= 31 && this.board[j] == 0) { 				// Range = [24, 25, 26, 27, 28, 29, 30, 31]
+			// En passant
+			if (24 <= i && i <= 31 && this.board[j] == 0) { 				// Range = [24, 25, 26, 27, 28, 29, 30, 31]
 				if (j - i == -9) {this.board[i-1] = 0;}
 				else if (j - i == -7) {this.board[i+1] = 0;}
 			
 			// Pawn promotion
-			} else if (0 <= j <= 7) {
+			} else if (0 <= j && j <= 7) {
 				this.#raw_move(i, j);
 				// Promote pawn -- give option
-				if (promotion == null) {console.log('Promoted! Assumed queen'); this.board[j] == 90;}
+				if (promotion == null) {console.log('White promoted! Assumed queen'); this.board[j] = 90;}
 				else {this.board[j] == promotion;}
 				return;
 			}
 		} else if (piece == -10) {
-			// En pasant
-			if (32 <= i <= 39 && this.board[j] == 0) {				// Range = [32, 33, 34, 35, 36, 37, 38, 39]
+			// En passant
+			if (32 <= i  && i <= 39 && this.board[j] == 0) {				// Range = [32, 33, 34, 35, 36, 37, 38, 39]
 				if (j - i == 7) {this.board[i-1] = 0;}
 				else if (j - i == 9) {this.board[i+1] = 0;}
 			
 			// Pawn promotion
-			} else if (56 <= j <= 63) {
+			} else if (56 <= j  && j <= 63) {
 				this.#raw_move(i, j);
 				// Promote pawn -- give option
-				if (promotion == null) {console.log('Promoted! Assumed queen'); this.board[j] == -90;}
+				if (promotion == null) {console.log('Black promoted! Assumed queen'); this.board[j] = -90;}
 				else {this.board[j] == promotion;}
 				return;
 			}
@@ -181,6 +184,27 @@ class Board {
 			let king_id = this.board.indexOf(player * 1000);
 			if (king_id == -1) {return true;} else {return false;}
 		} else {return false;}		// ERROR!
+	}
+
+	check_win() {
+		/**
+		Halt play once a king has been captured.
+		*/
+		let white = false, black = false;
+		for (let i = 0; i < this.board.length; i++) {
+			if (this.board[i] == 1000) {white = true;}
+			else if (this.board[i] == -1000) {black = true;}
+		}
+
+		if (white && black) {return;}
+		else {
+			let winner = null;
+			this.init_print(); 
+			if (white) {winner = "WHITE";}
+			else if (black) {winner = "BLACK"} 
+			alert(winner + " HAS WON!"); 
+			return;
+		}
 	}
 
 	// ============================== LEGAL MOVES ============================== //
@@ -331,7 +355,7 @@ class Board {
 			// While on board AND not capturing a friendly player
 			while (this.#on_board(j, move) && player != Math.sign(this.board[j + move])) {
 				legal.push(j + move);						// Add legal move
-				if (this.board[i] != 0) {break;}			// BREAK if capture enemy
+				if (this.board[j+move] != 0) {break;}			// BREAK if capture enemy
 				else if (max == 1) {break;}					// BREAK if can only move 1
 				j += move;									// Search from new position
 			}
@@ -357,8 +381,6 @@ class Board {
 		let legal = [];
 		let move = null;
 
-		i = parseInt(i)
-
 		switch (piece) {
 			case 0:
 				return [];
@@ -369,12 +391,12 @@ class Board {
 				if (this.board[i-8] == 0) {
 					legal.push(i-8);
 					// Check if you can jump two - valid on squares [48, 49, 50, 51, 52, 53, 54, 55]
-					if (48 <= i <= 55 && this.board[i-16] == 0) {legal.push(i-16);}
+					if (48 <= i && i <= 55 && this.board[i-16] == 0) {legal.push(i-16);}
 				}
 				// Add capture diagonal
 				if (i % 8 != 0 && this.board[i-9] < 0) {legal.push(i-9);}
 				if (i % 8 != 7 && this.board[i-7] < 0) {legal.push(i-7);}
-				// Add en pasant - valid on squares [24, 25, 26, 27, 28, 29, 30, 31]
+				// Add en passant - valid on squares [24, 25, 26, 27, 28, 29, 30, 31]
 				if (24 <= i <= 31) {
 					let a = this.prev_move[0], b = this.prev_move[1];
 					if (a == i - 17 && b == i - 1) {legal.push(i - 9);}
@@ -389,13 +411,13 @@ class Board {
 				if (this.board[i+8] == 0) {
 					legal.push(i + 8);
 					// Check double jump - valid on [ 8,  9, 10, 11, 12, 13, 14, 15]
-					if (8 <= i <= 15 && this.board[i+16] == 0) {legal.push(i+16);}
+					if (8 <= i && i <= 15 && this.board[i+16] == 0) {legal.push(i+16);}
 				}
 				// Capture diagonal
-				if (i % 8 != 0 && this.board[i+7] < 0) {legal.push(i+7);}
-				if (i % 8 != 7 && this.board[i+9] < 0) {legal.push(i+9);}
-				// En pasant - valid on [32, 33, 34, 35, 36, 37, 38, 39]
-				if (32 <= i <= 39) {
+				if (i % 8 != 0 && this.board[i+7] > 0) {legal.push(i+7);}
+				if (i % 8 != 7 && this.board[i+9] > 0) {legal.push(i+9);}
+				// En passant - valid on [32, 33, 34, 35, 36, 37, 38, 39]
+				if (32 <= i && i <= 39) {
 					let a = this.prev_move[0], b = this.prev_move[1];
 					if (a == i + 15 && b == i - 1) {legal.push(i + 7);}
 					else if (a == i + 17 && b == i + 1) {legal.push(i + 9);}
@@ -433,28 +455,37 @@ class Board {
 
 	// ============================== DISPLAY ============================== //
 	// Deprecate. Make it sub-class
-	stringify() {
+	#init_stringify() {
 		/**
 		Stringify this.board into HTML table.
 		*/
 		let string = "";
-		let letter = {0: '&nbsp', 10: 'P', 30: 'N', 31: 'B', 50: 'R', 90: 'Q', 1000: 'K', "-10": 'p', "-30": 'n', "-31": 'b', "-50": 'r', "-90": 'q', "-1000": 'k'}
+		let letter = {0: '&nbsp;', 10: 'P', 30: 'N', 31: 'B', 50: 'R', 90: 'Q', 1000: 'K', "-10": '<b>p</b>', "-30": '<b>n</b>', "-31": '<b>b</b>', "-50": '<b>r</b>', "-90": '<b>q</b>', "-1000": '<b>k</b>'};
 		let k = null;
 		for (let i = 0; i < 8; i++) {
 			string += "<tr>"
 			for (let j = 0; j < 8; j++) {
 				k = 8*i + j;
-				string += "<td id='"+ k + "'>" + letter[this.board[k]] + "</td>";
+				string += "<td id='"+ k + "-chess'>" + letter[this.board[k]] + "</td>";
 			}
 			string +=  "</tr>";
 		}
 		return string;
 	}
 
-	print(id="chess_board") {
+	init_print(id="chess_board") {
 		let element = document.getElementById(id);
-		let string = this.stringify();
+		let string = this.#init_stringify();
 		element.innerHTML = string;
+	}
+
+	print(id="chess_board") {
+		let element = null;
+		let letter = {0: '&nbsp;', 10: 'P', 30: 'N', 31: 'B', 50: 'R', 90: 'Q', 1000: 'K', "-10": '<b>p</b>', "-30": '<b>n</b>', "-31": '<b>b</b>', "-50": '<b>r</b>', "-90": '<b>q</b>', "-1000": '<b>k</b>'};
+		for (let i = 0; i < this.board.length; i++) {
+			element = document.getElementById(i + "-chess");
+			element.innerHTML = letter[this.board[i]];
+		}
 	}
 	
 	// ============================== AI PLAY ============================== //
